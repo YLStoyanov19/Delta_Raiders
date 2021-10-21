dd9fc2c0dcecb68f6a7ed24e2ca86d049a1a6042
Ivaylo Abadzhiev <IMAbadzhiev19@codingburgas.bg>
2021-10-21 16:52:02 +0300

    Add function to show menu

--- MazeProject/MazeProject/MazeProject.cpp
+++ MazeProject/MazeProject/MazeProject.cpp
@@ -13,6 +13,62 @@ void gotoXY(int x, int y)
 	SetConsoleCursorPosition(console, position);
 }
 
+void showMenu()
+{
+	bool flag = true;
+	int y = 2, choice = 0;
+
+	gotoXY(5, y - 2); cout << "Maze Menu";
+	gotoXY(5, y); cout << "->";
+
+	while (flag)
+	{
+		gotoXY(7, y); cout << "Game";
+		gotoXY(7, y + 1); cout << "Options";
+		gotoXY(7, y + 2); cout << "Quit";
+
+		system("pause>nul");
+
+		if (GetAsyncKeyState(VK_DOWN) && y != 4)
+		{
+			gotoXY(5, y); cout << "  ";
+			y++;
+			gotoXY(5, y); cout << "->";
+			choice++;
+			continue;
+		}
+
+		if (GetAsyncKeyState(VK_UP) && y != 2)
+		{
+			gotoXY(5, y); cout << "  ";
+			y--;
+			gotoXY(5, y); cout << "->";
+			choice--;
+			continue;
+		}
+
+		if (GetAsyncKeyState(VK_RETURN))
+		{
+			system("cls");
+
+			switch (choice)
+			{
+			case 0: {
+
+			}break;
+
+			case 1: {
+
+			}break;
+
+			case 2: {
+				exit(1);
+			}break;
+			}
+		}
+	}
+}//showMenu
+
 int main()
 {
 
